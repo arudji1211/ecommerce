a39ffd580ce19416a915240cc57c28b11473aac9
arudji1211 <lostarmy1211@gmail.com>
2024-10-31 15:05:16 +0700

    product repository

---
+++ module/repository/product/product_repository.go
@@ -0,0 +1,15 @@
+package product
+
+import (
+	"context"
+
+	"github.com/arudji1211/ecommerce/module/model/product"
+)
+
+type ProductRepository interface {
+	GetAll(ctx context.Context) (productOut []product.Product, err error)
+	GetByID(ctx context.Context, ID uint) (productOut product.Product, err error)
+	Create(ctx context.Context, productIn product.Product) (err error)
+	Update(ctx context.Context, productIn product.Product) (err error)
+	Delete(ctx context.Context, ID uint) (err error)
+}
---
+++ module/repository/product/product_repository_impl.go
@@ -0,0 +1,63 @@
+package product
+
+import (
+	"context"
+
+	"github.com/arudji1211/ecommerce/module/model/product"
+	"gorm.io/gorm"
+)
+
+type ProductRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func (p *ProductRepositoryImpl) GetAll(ctx context.Context) (productOut []product.Product, err error) {
+	//panic("not implemented") // TODO: Implement
+	tx := p.db.Model(product.Product{}).Find(productOut)
+	if err = tx.Error; err != nil {
+		return
+	}
+	return
+}
+
+func (p *ProductRepositoryImpl) GetByID(ctx context.Context, ID uint) (productOut product.Product, err error) {
+	//panic("not implemented") // TODO: Implement
+	tx := p.db.Model(product.Product{}).Where("id = ?", ID).Find(productOut)
+	if err = tx.Error; err != nil {
+		return
+	}
+	return
+}
+
+func (p *ProductRepositoryImpl) Create(ctx context.Context, productIn product.Product) (err error) {
+	//panic("not implemented") // TODO: Implement
+	tx := p.db.Model(product.Product{}).Create(productIn)
+	if err = tx.Error; err != nil {
+		return
+	}
+	return
+}
+
+func (p *ProductRepositoryImpl) Update(ctx context.Context, productIn product.Product) (err error) {
+	//panic("not implemented") // TODO: Implement
+	tx := p.db.Model(product.Product{}).Where("id = ?", productIn.ID).Updates(&productIn)
+	if err = tx.Error; err != nil {
+		return
+	}
+	return
+}
+
+func (p *ProductRepositoryImpl) Delete(ctx context.Context, ID uint) (err error) {
+	// panic("not implemented") // TODO: Implement
+	tx := p.db.Model(product.Product{}).Where("id = ?", ID).Delete(product.Product{})
+	if err = tx.Error; err != nil {
+		return
+	}
+	return
+}
+
+func NewProductRepository(DB *gorm.DB) ProductRepository {
+	return &ProductRepositoryImpl{
+		db: DB,
+	}
+}
