114094d6854854858325a5415d4b3b5f6a12987c
arudji1211 <lostarmy1211@gmail.com>
2024-11-05 20:13:26 +0700

    Product Service

--- module/service/product/product_service.go
+++ module/service/product/product_service.go
@@ -9,7 +9,7 @@ import (
 type ProductService interface {
 	GetAll(ctx context.Context) (Products []product.Product, err error)
 	GetByID(ctx context.Context, ID uint) (Product product.Product, err error)
-	Create(ctx context.Context, Product product.Product) (err error)
+	Create(ctx context.Context, ProductIn product.Product) (ProductOut product.Product, err error)
 	Update(ctx context.Context, Product product.Product) (err error)
 	Delete(ctx context.Context, ID uint) (err error)
 }
---
+++ module/service/product/product_service_impl.go
@@ -0,0 +1,63 @@
+package product
+
+import (
+	"context"
+
+	"github.com/arudji1211/ecommerce/module/model/product"
+	PRepo "github.com/arudji1211/ecommerce/module/repository/product"
+)
+
+type ProductServiceImpl struct {
+	ProductRepo PRepo.ProductRepository
+}
+
+func (p *ProductServiceImpl) GetAll(ctx context.Context) (Products []product.Product, err error) {
+	//panic("not implemented") // TODO: Implement
+	Products, err = p.ProductRepo.GetAll(ctx)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (p *ProductServiceImpl) GetByID(ctx context.Context, ID uint) (Product product.Product, err error) {
+	// panic("not implemented") // TODO: Implement
+	Product, err = p.ProductRepo.GetByID(ctx, ID)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (p *ProductServiceImpl) Create(ctx context.Context, ProductIn product.Product) (ProductOut product.Product, err error) {
+	// panic("not implemented") // TODO: Implement
+	ProductOut, err = p.ProductRepo.Create(ctx, ProductIn)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (p *ProductServiceImpl) Update(ctx context.Context, Product product.Product) (err error) {
+	//panic("not implemented") // TODO: Implement
+	err = p.ProductRepo.Update(ctx, Product)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (p *ProductServiceImpl) Delete(ctx context.Context, ID uint) (err error) {
+	//panic("not implemented") // TODO: Implement
+	err = p.ProductRepo.Delete(ctx, ID)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func NewProductService(repo PRepo.ProductRepository) ProductService {
+	return &ProductServiceImpl{
+		ProductRepo: repo,
+	}
+}
